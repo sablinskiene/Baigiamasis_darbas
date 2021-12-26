549a1d98a55bf2e75832f40c3fcf1dae906026ce
sablinskiene <l.sinkev@gmail.com>
2021-12-26 15:19:40 +0200

    Termometras nedirba.......

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 "use strict"
 
+//header
 
 let menuTrigger = document.getElementById("mobile-menu-trigger");
 let mobileMenu = document.querySelector("header nav");
@@ -8,3 +9,27 @@ menuTrigger.addEventListener('click', function() {
   this.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 });
+
+
+//Weather
+
+//my APIkey: 376aa1c5de2a9b773884484c9d294823
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Vilnius&units=metric&appid=376aa1c5de2a9b773884484c9d294823";
+const weatherElement = document.getElementById("weather-in-celsius");
+function getCurrentWeatherInCelsius() {
+    const http = new XMLHttpRequest();
+    http.open("GET", url);
+    http.addEventListener('load', function() {
+        const response = JSON.parse(http.response);
+        const temperature = response.main.temp;
+
+        if (temperature > 0) {
+            weatherElement.innerText = "+" + temperature.toFixed(0);
+        } else {
+            weatherElement.innerText = temperature.toFixed(0);
+        }
+    })
+    http.send();
+}
+window.addEventListener('load', getCurrentWeatherInCelsius)
